9c58d9cae17cafcd4dc0714294e0fabedfbfa64d
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2023-10-13 08:28:55 -0300

    fix: limite de posts e criando condicional para validar se é necessario a divisão do texto

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from reddit_client import reddit_client
 from services import *
 
 subreddit = reddit_client.subreddit(SUBREDDIT)
-top_posts = subreddit.top(time_filter=TIME_FILTER, limit=1)
+top_posts = subreddit.top(time_filter=TIME_FILTER, limit=LIMIT_OF_POSTS)
 
 for post in top_posts:
-    text_parts = split_text(post.selftext)
-    translated_text_parts = [translate_text(text) for text in text_parts]
+    final_text = ""
+
+    if (len(post.selftext) > 500):
+        text_parts = split_text(post.selftext)
+        translated_text_parts = [translate_text(text) for text in text_parts]
+        final_text = join_text(translated_text_parts)
+
+    final_text = translate_text(post.selftext)
     final_title = translate_text(post.title)
-    final_text = join_text(translated_text_parts)
-    print(final_title + "\n" + final_text)
