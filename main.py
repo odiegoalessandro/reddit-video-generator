db61440035f4ecc7f628eb2f64325c20371d776d
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2023-10-13 10:09:46 -0300

    chore: alterando a biblioteca de tradução e simplificando o codigo

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ top_posts = subreddit.top(time_filter=TIME_FILTER, limit=LIMIT_OF_POSTS)
 for post in top_posts:
     final_text = ""
 
-    if (len(post.selftext) > 500):
-        text_parts = split_text(post.selftext)
-        translated_text_parts = [translate_text(text) for text in text_parts]
-        final_text = join_text(translated_text_parts)
+    # if (len(post.selftext) >= 500):
+    #     text_parts = split_text(post.selftext)
+    #     translated_text_parts = [translate_text(text) for text in text_parts]
+    #     final_text = join_text(translated_text_parts)
 
     final_text = translate_text(post.selftext)
     final_title = translate_text(post.title)
--- services.py
+++ services.py
@@ -1,32 +1,31 @@
-from translate import Translator
+from googletrans import Translator
 
 from constants import *
 
 
 def translate_text(text):
-    translator = Translator(to_lang=LANGUAGE)
+    translator = Translator()
+    translated_text = translator.translate(text, LANGUAGE, "en")
+    return translated_text.text
 
-    return translator.translate(text)
+# def split_text(text):
+#     segments = []
+#     current_segment = ""
+#     text_words = text.split()
+
+#     for word in text_words:
+#         if len(current_segment) + len(word) > LIMIT_OF_CHARS:
+#             segments.append(current_segment)
+#             current_segment = word
+#         else:
+#             current_segment += (" " if current_segment else "") + word
+
+#     if current_segment:
+#         segments.append(current_segment)
+
+#     return segments
 
 
-def split_text(text):
-    segments = []
-    current_segment = ""
-    text_words = text.split()
-
-    for word in text_words:
-        if len(current_segment) + len(word) > LIMIT_OF_CHARS:
-            segments.append(current_segment)
-            current_segment = word
-        else:
-            current_segment += (" " if current_segment else "") + word
-
-    if current_segment:
-        segments.append(current_segment)
-
-    return segments
-
-
-def join_text(text_parts):
-    complete_text = "".join(text_parts)
-    return complete_text
+# def join_text(text_parts):
+#     complete_text = "".join(text_parts)
+#     return complete_text
