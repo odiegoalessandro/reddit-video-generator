205aa09f693b088af42bfe520ef6315ba15b09e3
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2023-10-16 22:46:16 -0300

    feat: melhorando a logica do tts e estruturando a main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from constants import *
 from reddit.client import reddit_client
 from reddit.subreddit import get_subreddit_thread
+from services.tts import text_to_mp3
 from services.utils import *
 
 subreddit = reddit_client.subreddit(SUBREDDIT)
 top_posts = subreddit.top(time_filter=TIME_FILTER, limit=LIMIT_OF_POSTS)
 
 if __name__=="__main__":    
-    for post in top_posts:
-        print(get_subreddit_thread(post.id))
+    for index, post in enumerate(top_posts):
+        post_thread = get_subreddit_thread(post.id)
+        text_to_mp3(filename=f"{index}-title", text=post.title)
+        text_to_mp3(filename=f"{index}-body", text=post.selftext)
+        # criar a logica para obter os comentarios em formato de mp3
--- services/tts.py
+++ services/tts.py
@@ -1,15 +1,21 @@
+import os
+
 from gtts import gTTS
 
-from utils import clean_text, translate_text
+from services.utils import clean_text, translate_text
 
 
-def text_to_mp3(filename, text, language):
-    if(language != "pt-br"):
-        translated_text = translate_text(clean_text(text))
+def text_to_mp3(filename, text, language="pt-br"):
+    if text:
+        final_text = text
         
-        tts = gTTS(text=translated_text, lang="pt-br", slow=False, lang_check=True)
-        tts.save(f"{filename}.mp3")
+        if(language != "pt-br"):
+            final_text = translate_text(clean_text(text))
+            
+        if not os.path.exists(os.path.join(os.getcwd(), "voices")):
+            os.mkdir(os.path.join(os.getcwd(), "voices"))
+        
+        tts = gTTS(text=clean_text(final_text), lang="pt-br", slow=False, lang_check=True)
+        tts.save(f"{os.path.join(os.getcwd(), "voices")}/{filename}.mp3")
     else:
-
-        tts = gTTS(text=clean_text(text), lang="pt-br", slow=False, lang_check=True)
-        tts.save(f"{filename}.mp3")
+        print("Parece que esse texto está vazio :(")
