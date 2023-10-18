ea2d57f422e10e202511799a3a22a9c049d38c1f
Diego alessandro da cruz martins <diegoalessandrodacruzmartins@gmail.com>
2023-10-18 07:41:19 -0300

    refactor: limpando parte do codigo e removendo repetições de codigo desnecessarias

--- services/tts.py
+++ services/tts.py
@@ -8,18 +8,19 @@ from services.utils import clean_text, translate_text
 
 def text_to_mp3(filename, text, language="pt-br"):
     if text:
-        final_text = text
+        final_text = clean_text(text)
+        voices_path = os.path.join(os.getcwd(), "voices")
 
         if (language != "pt-br"):
-            final_text = translate_text(clean_text(text))
+            final_text = translate_text(final_text)
 
-        if not os.path.exists(os.path.join(os.getcwd(), "voices")):
+        if not os.path.exists(voices_path):
             print_warning("Criando diretorio /voices...")
-            os.mkdir(os.path.join(os.getcwd(), "voices"))
+            os.mkdir(voices_path)
 
-        tts = gTTS(text=clean_text(final_text),
+        tts = gTTS(text=final_text,
                    lang="pt-br", slow=False, lang_check=True)
-        tts.save(f"{os.path.join(os.getcwd(), "voices")}/{filename}.mp3")
+        tts.save(f"{voices_path}/{filename}.mp3")
     else:
         print_warning(
             "Aviso! a variavel text está vazia, não foi possivel criar o arquivo .mp3")
